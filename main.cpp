17eaef03d707da5515444bde9342183f181f599a
Omer13 <omerjawhar@live.com>
2025-03-19 02:53:04 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+# include "window.h"    // include window header
+
+int main()              // main function
+{
+    Window window;      // Create an instance of the window class
+    window.run();       // run the run member function
+    return 0;           
+}
